e66ab28ab07af25d7aeef071ee11fe64f870d693
ArielMobileLab <76939624+ArielMobileLab@users.noreply.github.com>
2024-01-17 14:35:43 +0200

    Update tidy.py

--- Andromeda/tidy.py
+++ Andromeda/tidy.py
@@ -287,7 +287,7 @@ def tidy_carla(path):
         # df=pd.DataFrame.from_dict(df, orient='columns')
         df = df.rename(columns={"Reson": "Reason"})
         df = df.rename(columns={"Simulation_time": "SimulationTime"})
-        df["WorldTime"]=df["Timestamp"]
+        df["WorldTime"]=df["Timestamp"].str.slice(11,19)
 ### GPS messages
         GPS=df[df.Type=="Ego car Sensors:"]
         GPS=GPS.dropna(axis=1, how='all')
